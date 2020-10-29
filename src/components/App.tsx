f0504b2ee423cba29390c94d3eb0c324f14c9f8f
Jason Howmans <jason@nosaj.io>
2020-10-29 22:18:28 +0000

    Initial commit -- base project 🚀

---
+++ src/components/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import formInstructions from '../data/form_instructions.json';
+
+function App() {
+  // Check your console to see the full instructions!
+  console.log(formInstructions);
+
+  return (
+    <div>
+      <img src="https://frontier-public-assets.s3-us-west-2.amazonaws.com/frontier-corona-logo.svg" alt="Frontier Logo" />
+      <h1>👋 Hello from Team Frontier!</h1>
+      <p>Good luck with the exercise. If you have any questions please email Jason: jason@frontier.jobs</p>
+    </div>
+  );
+}
+
+export default App;
